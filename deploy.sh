906dcf59b9e5762dc49d1d95d31a3ff3610da697
Thibaut Lorrain <tlorrain@excilys.com>
2015-11-24 16:43:04 +0100

    fix GH_REF env variable

--- deploy.sh
+++ deploy.sh
@@ -10,3 +10,4 @@ git commit -m "Deploy last h2geo version"
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+
